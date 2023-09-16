2ce4fea1cd8e87e0773619778d23749d6653f460
Amit Kashte <amitkashte93@gmail.com>
2023-09-17 01:22:30 +0530

    form validation

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,11 +6,26 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function index(){
-        return view('pages.login');
-    }
+  public function index()
+  {
+    return view('pages.login');
+  }
 
-    public function loginHandler(Request $request){
-        dd($request->all());
-    }
+  public function loginHandler(Request $request)
+  {
+
+    $request->validate(
+      [
+        'name' => 'required|alpha|min:6',
+        'email' => ['required', 'email'],
+        'password' => ['required', 'email', 'min:8', 'max:12'],
+      ],
+      [
+        'name.required' => 'User name field is mandatory.',
+        'name.alpha' => 'User name must contain letters.',
+      ]
+    );
+
+    return $request;
+  }
 }
